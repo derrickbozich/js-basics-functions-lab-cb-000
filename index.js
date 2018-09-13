126989839bf9666c95a6709aedbe0b41994c6ed1
derrick b <dbozich1@gmail.com>
2018-09-13 20:12:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function calculatesFarePrice(start, destination){
 
   if (dist < 400) {
     return 0;
-  } else (dist > 400 && dist < 2000) {
+  } else if (dist > 400 && dist < 2000) {
     return (dist-400) * .02;
   }
 
